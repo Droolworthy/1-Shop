0e6f5e1c353bc512e0564cb67e877d21646d605c
Droolworthy <107348019+Droolworthy@users.noreply.github.com>
2022-06-14 13:59:07 +0300

    Добавьте файлы проекта.

---
+++ CS6/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace CS6
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int initialAmount;
+            int amountGold;
+            int crystalPrice = 10;
+            bool enoughMoney;
+            Console.Write("Введите кол-во золота: ");
+            initialAmount = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Предлагаю вам купить 10 кристаллов по цене 10 золота за штуку: ");
+            amountGold = Convert.ToInt32(Console.ReadLine());
+            enoughMoney = initialAmount >= amountGold * crystalPrice;
+            amountGold *= Convert.ToInt32(enoughMoney);
+            initialAmount -= amountGold * crystalPrice;
+            Console.WriteLine("Поздравляю! Вы приобрели: " + amountGold + " кристаллов");
+            Console.WriteLine("У вас осталось: " + initialAmount + " золота");
+        }
+    }
+}
