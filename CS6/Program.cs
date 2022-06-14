f1e7a031dc92c824a7e29132f8279eec2edf3e54
Droolworthy <107348019+Droolworthy@users.noreply.github.com>
2022-06-14 14:21:47 +0300

    Update Program.cs

--- CS6/Program.cs
+++ CS6/Program.cs
@@ -7,17 +7,17 @@ namespace CS6
         static void Main(string[] args)
         {
             int initialamountGold;
-            int amountGold;
+            int numberСrystals;
             int crystalPrice = 10;
             bool enoughMoney;
             Console.Write("Введите кол-во золота: ");
             initialamountGold = Convert.ToInt32(Console.ReadLine());
             Console.Write("Предлагаю вам купить 10 кристаллов по цене 10 золота за штуку: ");
-            amountGold = Convert.ToInt32(Console.ReadLine());
-            enoughMoney = initialamountGold >= amountGold * crystalPrice;
-            amountGold *= Convert.ToInt32(enoughMoney);
-            initialamountGold -= amountGold * crystalPrice;
-            Console.WriteLine("Поздравляю! Вы приобрели: " + amountGold + " кристаллов");
+            numberСrystals = Convert.ToInt32(Console.ReadLine());
+            enoughMoney = initialamountGold >= numberСrystals * crystalPrice;
+            numberСrystals *= Convert.ToInt32(enoughMoney);
+            initialamountGold -= numberСrystals * crystalPrice;
+            Console.WriteLine("Поздравляю! Вы приобрели: " + numberСrystals + " кристаллов");
             Console.WriteLine("У вас осталось: " + initialamountGold + " золота");
         }
     }
